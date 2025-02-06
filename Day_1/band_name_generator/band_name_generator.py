562fc9658f77d857e3294b818fb889acaccbbb5a
ayush <ayush16.mitmpl2023@learner.manipal.edu>
2025-02-06 20:58:03 +0530

    In this first day, I wrote the beginner code called thehello world which is a standard basic code the people write when they start a programming language and i aslo wrote another program which called 'band name generator' using basics of string.

---
+++ Day_1/band_name_generator/band_name_generator.py
@@ -0,0 +1,7 @@
+#welcome to band name generator
+#A simple program that uses print and input funtion and also the concept of string concatenation and fstrings
+print("Welcome to Band Name Generator program")
+city_name = input("Enter the name of the city that you grew up in:\n")
+pet_name = input("Enter the name of the name of your pet:\n")
+print(f"The name of your band is {city_name} {pet_name}")
+
